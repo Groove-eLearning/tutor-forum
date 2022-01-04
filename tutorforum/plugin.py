604c24e183e8654e5a99ac421fc313735da75478
Huy Nguyen <huynguyen.kh@groovetechnology.com>
2022-01-04 11:20:43 +0700

    Updating settings and Dockerfile

--- tutorforum/plugin.py
+++ tutorforum/plugin.py
@@ -11,7 +11,7 @@ config = {
     "add": {},
     "defaults": {
         "VERSION": __version__,
-        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-forum:{{ FORUM_VERSION }}",
+        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}edutechdev/openedx-forum:{{ FORUM_VERSION }}",
         "MONGODB_DATABASE": "cs_comments_service",
         "PORT": "4567",
         "API_KEY": "forumapikey",
